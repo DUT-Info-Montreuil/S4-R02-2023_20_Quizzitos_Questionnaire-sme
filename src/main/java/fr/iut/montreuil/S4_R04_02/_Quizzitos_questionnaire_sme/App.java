a042d0afed294ad29f6672e682a4449e0fda1d64
Cyril Grosjean <cyril.grosjean@outlook.fr>
2023-05-09 14:49:03 +0200

    ajout de l'architecture du projet

---
+++ src/main/java/fr/iut/montreuil/S4_R04_02/_Quizzitos_questionnaire_sme/App.java
@@ -0,0 +1,13 @@
+package fr.iut.montreuil.S4_R04_02._Quizzitos_questionnaire_sme;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
